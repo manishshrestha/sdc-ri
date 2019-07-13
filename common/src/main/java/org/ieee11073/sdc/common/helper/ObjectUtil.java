12c93daa2cec9634c05f3e7b10abbf3b0761ec3b
David Gregorczyk <david.gregorczyk@web.de>
2019-07-13 15:55:40 +0200

    Add utiltiy function to create immutable facades.

--- common/src/main/java/org/ieee11073/sdc/common/helper/ObjectUtil.java
+++ common/src/main/java/org/ieee11073/sdc/common/helper/ObjectUtil.java
@@ -1,13 +1,26 @@
 package org.ieee11073.sdc.common.helper;
 
+
 /**
  * Utility class to deep copy arbitrary objects.
  */
 public interface ObjectUtil {
     /**
-     * Create deep copy of given object
+     * Create deep copy of given object.
+     *
      * @param obj Object to copy.
-     * @return Deep copy of given object or null if given object is null.
+     * @return Deep copy of given object.
+     * @throws RuntimeException In case object could not be deep-copied.
      */
     <T> T deepCopy(T obj);
+
+    /**
+     * Given a getter/setter bean, this function creates an immutable facade that throws runtime exception when tried
+     * to be modified.
+     *
+     * <b>Attention: implementations may reject instances of inner classes!</b>
+     *
+     * @throws RuntimeException In case object facade could not be created.
+     */
+    <C> C immutableFacade(C instance);
 }
--- common/src/main/java/org/ieee11073/sdc/common/helper/ObjectUtilImpl.java
+++ common/src/main/java/org/ieee11073/sdc/common/helper/ObjectUtilImpl.java
@@ -1,6 +1,7 @@
 package org.ieee11073.sdc.common.helper;
 
 import com.google.inject.Inject;
+import com.javax0.immutator.Immutable;
 import com.rits.cloning.Cloner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,15 +20,20 @@ public class ObjectUtilImpl implements ObjectUtil {
 
     @Override
     public <T> T deepCopy(T obj) {
-        if (obj == null) {
-            return null;
-        }
-
         try {
             return cloner.deepClone(obj);
         } catch (Exception e) {
             LOG.warn("Unable to deepCopy object due to: {}", e.getMessage());
         }
         throw new RuntimeException("Fatal error on object deepCopy");
+    }
+
+    @Override
+    public <C> C immutableFacade(C instance) {
+        try {
+            return Immutable.of(instance);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
