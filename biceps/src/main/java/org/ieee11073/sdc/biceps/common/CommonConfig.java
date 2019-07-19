6710a8f9ae9ca22dd5ba5d7d42198bc507b8bae7
David Gregorczyk <david.gregorczyk@web.de>
2019-07-19 13:30:44 +0200

    Add configuration for common package to BICEPS including default values.

---
+++ biceps/src/main/java/org/ieee11073/sdc/biceps/common/CommonConfig.java
@@ -0,0 +1,14 @@
+package org.ieee11073.sdc.biceps.common;
+
+/**
+ * General configuration of BICEPS's common package.
+ */
+public class CommonConfig {
+    /**
+     * Control maximum queue size of MDIB queues.
+     * <p>
+     * - Data type: {@linkplain Integer}
+     * - Use: optional
+     */
+    public static final String MDIB_QUEUE_SIZE = "Biceps.Common.MdibQueueSize";
+}
---
+++ biceps/src/main/java/org/ieee11073/sdc/biceps/guice/DefaultBicepsModule.java
@@ -0,0 +1,24 @@
+package org.ieee11073.sdc.biceps.guice;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.assistedinject.FactoryModuleBuilder;
+import org.ieee11073.sdc.biceps.common.*;
+import org.ieee11073.sdc.biceps.common.factory.MdibEntityFactory;
+import org.ieee11073.sdc.biceps.common.factory.MdibQueueFactory;
+
+/**
+ * Guice module that determines default values for BICEPS package configuration.
+ */
+public class DefaultBicepsModule extends AbstractModule {
+    @Override
+    protected void configure() {
+        install(new FactoryModuleBuilder()
+                .implement(WritableMdibEntity.class, MdibEntityImpl.class)
+                .implement(MdibEntity.class, MdibEntityImpl.class)
+                .build(MdibEntityFactory.class));
+
+        install(new FactoryModuleBuilder()
+                .implement(MdibQueue.class, MdibQueueImpl.class)
+                .build(MdibQueueFactory.class));
+    }
+}
