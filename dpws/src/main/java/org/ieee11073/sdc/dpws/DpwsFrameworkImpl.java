c5187ee1aae0896bc453bf71572480ffb1b8cb66
David Gregorczyk <david.gregorczyk@web.de>
2019-09-05 23:33:05 +0200

    #20 Facilitate defining the network adapter to be used for UDP.

--- dpws/src/main/java/org/ieee11073/sdc/dpws/DpwsFrameworkImpl.java
+++ dpws/src/main/java/org/ieee11073/sdc/dpws/DpwsFrameworkImpl.java
@@ -3,6 +3,8 @@ package org.ieee11073.sdc.dpws;
 import com.google.common.util.concurrent.AbstractIdleService;
 import com.google.common.util.concurrent.ServiceManager;
 import com.google.inject.Inject;
+import com.google.inject.assistedinject.Assisted;
+import com.google.inject.assistedinject.AssistedInject;
 import org.ieee11073.sdc.dpws.udp.factory.UdpBindingServiceFactory;
 import org.ieee11073.sdc.dpws.http.HttpServerRegistry;
 import org.ieee11073.sdc.dpws.soap.SoapMarshalling;
@@ -12,7 +14,10 @@ import org.ieee11073.sdc.dpws.udp.UdpMessageQueueService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nullable;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.Arrays;
 
@@ -22,6 +27,7 @@ import java.util.Arrays;
 public class DpwsFrameworkImpl extends AbstractIdleService implements DpwsFramework {
     private static final Logger LOG = LoggerFactory.getLogger(DpwsFrameworkImpl.class);
 
+    private NetworkInterface networkInterface;
     private final UdpMessageQueueService udpMessageQueueService;
     private final UdpBindingServiceFactory udpBindingServiceFactory;
     private final HttpServerRegistry httpServerRegistry;
@@ -30,11 +36,21 @@ public class DpwsFrameworkImpl extends AbstractIdleService implements DpwsFramew
     private ServiceManager serviceManager;
     private UdpBindingService udpBindingService;
 
-    @Inject
+    @AssistedInject
     DpwsFrameworkImpl(@DiscoveryUdpQueue UdpMessageQueueService udpMessageQueueService,
                       UdpBindingServiceFactory udpBindingServiceFactory,
                       HttpServerRegistry httpServerRegistry,
                       SoapMarshalling soapMarshalling) {
+        this(null, udpMessageQueueService, udpBindingServiceFactory, httpServerRegistry, soapMarshalling);
+    }
+
+    @AssistedInject
+    DpwsFrameworkImpl(@Assisted @Nullable NetworkInterface networkInterface,
+                      @DiscoveryUdpQueue UdpMessageQueueService udpMessageQueueService,
+                      UdpBindingServiceFactory udpBindingServiceFactory,
+                      HttpServerRegistry httpServerRegistry,
+                      SoapMarshalling soapMarshalling) {
+        this.networkInterface = networkInterface;
         this.udpMessageQueueService = udpMessageQueueService;
         this.udpBindingServiceFactory = udpBindingServiceFactory;
         this.httpServerRegistry = httpServerRegistry;
@@ -44,6 +60,11 @@ public class DpwsFrameworkImpl extends AbstractIdleService implements DpwsFramew
     @Override
     protected void startUp() throws Exception {
         LOG.info("Start SDCri DPWS framework.");
+
+        if (networkInterface == null) {
+            networkInterface = NetworkInterface.getByInetAddress(InetAddress.getLocalHost());
+        }
+
         configureDiscovery();
         serviceManager = new ServiceManager(Arrays.asList(udpBindingService, udpMessageQueueService,
                 httpServerRegistry, soapMarshalling));
@@ -58,7 +79,7 @@ public class DpwsFrameworkImpl extends AbstractIdleService implements DpwsFramew
         LOG.info("SDCri DPWS framework shut down.");
     }
 
-    private void configureDiscovery() {
+    private void configureDiscovery() throws UnknownHostException, SocketException {
         InetAddress wsdMulticastAddress;
         try {
             wsdMulticastAddress = InetAddress.getByName(WsDiscoveryConstants.IPV4_MULTICAST_ADDRESS);
@@ -67,8 +88,11 @@ public class DpwsFrameworkImpl extends AbstractIdleService implements DpwsFramew
             throw new RuntimeException(e);
         }
 
-        udpBindingService = udpBindingServiceFactory.createUdpBindingService(wsdMulticastAddress,
-                DpwsConstants.DISCOVERY_PORT, DpwsConstants.MAX_UDP_ENVELOPE_SIZE);
+        udpBindingService = udpBindingServiceFactory.createUdpBindingService(
+                networkInterface,
+                wsdMulticastAddress,
+                DpwsConstants.DISCOVERY_PORT,
+                DpwsConstants.MAX_UDP_ENVELOPE_SIZE);
         udpMessageQueueService.setUdpBinding(udpBindingService);
         udpBindingService.setMessageReceiver(udpMessageQueueService);
     }
