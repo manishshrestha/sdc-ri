4a42ea00bfc27f3d78551019276e4ee8f61777ae
David Gregorczyk <david.gregorczyk@web.de>
2019-10-05 13:24:19 +0200

    #36 Add base packages and classes to the glue package.

---
+++ glue/src/main/java/org/ieee11073/sdc/glue/common/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Implementation of the SDC provider.
+ */
+@ParametersAreNonnullByDefault
+package org.ieee11073.sdc.glue.common;
+
+import javax.annotation.ParametersAreNonnullByDefault;
---
+++ glue/src/main/java/org/ieee11073/sdc/glue/provider/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Classes shared between the SDC provider and consumer side.
+ */
+@ParametersAreNonnullByDefault
+package org.ieee11073.sdc.glue.provider;
+
+import javax.annotation.ParametersAreNonnullByDefault;
