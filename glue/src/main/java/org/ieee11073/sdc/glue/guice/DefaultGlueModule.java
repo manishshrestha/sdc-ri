7846608b780d2b8545e02b4428988f3d1b4be010
David Gregorczyk <david.gregorczyk@web.de>
2019-11-05 19:57:18 +0100

    #38 Cleanup of Glue Guice module.

--- glue/src/main/java/org/ieee11073/sdc/glue/guice/DefaultGlueModule.java
+++ glue/src/main/java/org/ieee11073/sdc/glue/guice/DefaultGlueModule.java
@@ -19,8 +19,15 @@ import org.ieee11073.sdc.biceps.consumer.access.factory.RemoteMdibAccessFactory;
 import org.ieee11073.sdc.biceps.provider.access.LocalMdibAccess;
 import org.ieee11073.sdc.biceps.provider.access.LocalMdibAccessImpl;
 import org.ieee11073.sdc.biceps.provider.access.factory.LocalMdibAccessFactory;
+import org.ieee11073.sdc.glue.provider.SdcServices;
+import org.ieee11073.sdc.glue.provider.factory.SdcServicesFactory;
+import org.ieee11073.sdc.glue.provider.sco.Context;
+import org.ieee11073.sdc.glue.provider.sco.ScoController;
 import org.ieee11073.sdc.glue.provider.sco.factory.ContextFactory;
 import org.ieee11073.sdc.glue.provider.sco.factory.ScoControllerFactory;
+import org.ieee11073.sdc.glue.provider.services.HighPriorityServices;
+import org.ieee11073.sdc.glue.provider.services.factory.ServicesFactory;
+import org.ieee11073.sdc.glue.provider.services.helper.MdibMapper;
 import org.ieee11073.sdc.glue.provider.services.helper.ReportGenerator;
 import org.ieee11073.sdc.glue.provider.services.helper.factory.MdibMapperFactory;
 import org.ieee11073.sdc.glue.provider.services.helper.factory.ReportGeneratorFactory;
@@ -32,19 +39,27 @@ public class DefaultGlueModule extends AbstractModule {
     @Override
     protected void configure() {
         install(new FactoryModuleBuilder()
+                .implement(ReportGenerator.class, ReportGenerator.class)
                 .build(ReportGeneratorFactory.class));
 
         install(new FactoryModuleBuilder()
+                .implement(MdibMapper.class, MdibMapper.class)
                 .build(MdibMapperFactory.class));
 
         install(new FactoryModuleBuilder()
+                .implement(Context.class, Context.class)
                 .build(ContextFactory.class));
 
         install(new FactoryModuleBuilder()
+                .implement(ScoController.class, ScoController.class)
                 .build(ScoControllerFactory.class));
-//
-//        install(new FactoryModuleBuilder()
-//                .implement(MdibStorage.class, MdibStorageImpl.class)
-//                .build(MdibStorageFactory.class));
+
+        install(new FactoryModuleBuilder()
+                .implement(HighPriorityServices.class, HighPriorityServices.class)
+                .build(ServicesFactory.class));
+
+        install(new FactoryModuleBuilder()
+                .implement(SdcServices.class, SdcServices.class)
+                .build(SdcServicesFactory.class));
     }
 }
