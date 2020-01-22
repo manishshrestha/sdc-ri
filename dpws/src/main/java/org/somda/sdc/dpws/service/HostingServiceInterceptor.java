f5dd10a4437326ac0439cc7cb958a64bb392941c
David Gregorczk <david.gregorczyk@web.de>
2020-01-22 22:58:38 +0100

    #42 Implement and test DPWS MAX_FIELD_SIZE and MAX_URI_SIZE.

--- dpws/src/main/java/org/somda/sdc/dpws/service/HostingServiceInterceptor.java
+++ dpws/src/main/java/org/somda/sdc/dpws/service/HostingServiceInterceptor.java
@@ -2,9 +2,13 @@ package org.somda.sdc.dpws.service;
 
 import com.google.inject.assistedinject.Assisted;
 import com.google.inject.assistedinject.AssistedInject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.somda.sdc.common.util.ObjectUtil;
 import org.somda.sdc.dpws.DpwsConstants;
-import org.somda.sdc.dpws.model.*;
+import org.somda.sdc.dpws.model.LocalizedStringType;
+import org.somda.sdc.dpws.model.ThisDeviceType;
+import org.somda.sdc.dpws.model.ThisModelType;
 import org.somda.sdc.dpws.service.helper.MetadataSectionUtil;
 import org.somda.sdc.dpws.soap.SoapUtil;
 import org.somda.sdc.dpws.soap.exception.SoapFaultException;
@@ -18,11 +22,12 @@ import org.somda.sdc.dpws.soap.wsmetadataexchange.model.Metadata;
 import org.somda.sdc.dpws.soap.wsmetadataexchange.model.MetadataSection;
 import org.somda.sdc.dpws.soap.wsmetadataexchange.model.ObjectFactory;
 import org.somda.sdc.dpws.soap.wstransfer.WsTransferConstants;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nullable;
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -35,7 +40,7 @@ public class HostingServiceInterceptor implements HostingService {
     private static final Logger LOG = LoggerFactory.getLogger(HostingServiceInterceptor.class);
 
     private final WsDiscoveryTargetService targetService;
-    private final ObjectUtil beanUtil;
+    private final ObjectUtil objectUtil;
     private final WsAddressingUtil wsaUtil;
     private final MetadataSectionUtil metadataSectionUtil;
     private final List<HostedService> hostedServices;
@@ -59,11 +64,11 @@ public class HostingServiceInterceptor implements HostingService {
                               SoapFaultFactory soapFaultFactory,
                               ObjectFactory mexFactory,
                               org.somda.sdc.dpws.model.ObjectFactory dpwsFactory,
-                              ObjectUtil beanUtil,
+                              ObjectUtil objectUtil,
                               WsAddressingUtil wsaUtil,
                               MetadataSectionUtil metadataSectionUtil) {
         this.targetService = targetService;
-        this.beanUtil = beanUtil;
+        this.objectUtil = objectUtil;
         this.wsaUtil = wsaUtil;
         this.metadataSectionUtil = metadataSectionUtil;
         this.hostedServices = new ArrayList<>();
@@ -131,23 +136,32 @@ public class HostingServiceInterceptor implements HostingService {
 
     @Override
     public ThisModelType getThisModel() {
-        return beanUtil.deepCopy(thisModel);
+        return objectUtil.deepCopy(thisModel);
     }
 
     @Override
     public void setThisModel(ThisModelType thisModel) {
-        this.thisModel = beanUtil.deepCopy(thisModel);
+        this.thisModel = objectUtil.deepCopy(thisModel);
+        this.thisModel.setManufacturer(cutMaxFieldSize(thisModel.getManufacturer()));
+        this.thisModel.setModelName(cutMaxFieldSize(thisModel.getModelName()));
+        this.thisModel.setModelNumber(cutMaxFieldSize(thisModel.getModelNumber()));
+        this.thisModel.setManufacturerUrl(cutMaxUriSize(thisModel.getManufacturerUrl()));
+        this.thisModel.setPresentationUrl(cutMaxUriSize(thisModel.getPresentationUrl()));
+        this.thisModel.setModelUrl(cutMaxUriSize(thisModel.getModelUrl()));
         targetService.setMetadataModified();
     }
 
     @Override
     public ThisDeviceType getThisDevice() {
-        return beanUtil.deepCopy(thisDevice);
+        return objectUtil.deepCopy(thisDevice);
     }
 
     @Override
     public void setThisDevice(ThisDeviceType thisDevice) {
-        this.thisDevice = beanUtil.deepCopy(thisDevice);
+        this.thisDevice = objectUtil.deepCopy(thisDevice);
+        this.thisDevice.setFriendlyName(cutMaxFieldSize(thisDevice.getFriendlyName()));
+        this.thisDevice.setFirmwareVersion(cutMaxFieldSize(thisDevice.getFirmwareVersion()));
+        this.thisDevice.setSerialNumber(cutMaxFieldSize(thisDevice.getSerialNumber()));
         targetService.setMetadataModified();
     }
 
@@ -169,5 +183,40 @@ public class HostingServiceInterceptor implements HostingService {
             str += " (" + getThisDevice().getFriendlyName().get(0).getValue() + ")";
         }
         return str;
+    }
+
+    private List<LocalizedStringType> cutMaxFieldSize(List<LocalizedStringType> texts) {
+        var newList = new ArrayList<LocalizedStringType>(texts.size());
+        for (var text : texts) {
+            var localizedText = objectUtil.deepCopy(text);
+            localizedText.setValue(cutMaxFieldSize(text.getValue()));
+            newList.add(localizedText);
+        }
+
+        return newList;
+    }
+
+    private String cutMaxFieldSize(@Nullable String text) {
+        return cutSize(text, DpwsConstants.MAX_FIELD_SIZE);
+    }
+
+    private String cutMaxUriSize(@Nullable String uri) {
+        return cutSize(uri, DpwsConstants.MAX_URI_SIZE);
+    }
+
+    private String cutSize(@Nullable String text, int size) {
+        if (text == null) {
+            return null;
+        }
+        if (text.getBytes(StandardCharsets.UTF_8).length >= size) {
+            int maxLength = size - 1;
+            var newText = new String(Arrays.copyOf(text.getBytes(StandardCharsets.UTF_8), maxLength),
+                    StandardCharsets.UTF_8);
+            LOG.warn("The following text was cut due to DPWS length violations (allowed: {} octets, " +
+                    "actual: {} octets). '{}' is now '{}'", maxLength, text.getBytes().length, text, newText);
+            return newText;
+        } else {
+            return text;
+        }
     }
 }
