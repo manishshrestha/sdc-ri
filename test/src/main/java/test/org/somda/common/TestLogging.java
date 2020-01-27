1f63e18c92986ba3a532170fee0b26979ef6b51d
Lukas Deichmann <lukasdeichmann+gitlab@gmail.com>
2020-01-27 12:29:42 +0100

    Make wire be quiet when running tests.

--- test/src/main/java/test/org/somda/common/TestLogging.java
+++ test/src/main/java/test/org/somda/common/TestLogging.java
@@ -1,11 +1,8 @@
 package test.org.somda.common;
 
 import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.core.appender.FileAppender;
 import org.apache.logging.log4j.core.config.Configurator;
 import org.apache.logging.log4j.core.config.DefaultConfiguration;
-
-import java.io.File;
 
 /**
  * Utility class with static logging configuration for tests.
@@ -16,19 +13,9 @@ public class TestLogging {
      */
 
     public static void configure() {
-        var cfg = new DefaultConfiguration();
-
-        // this will create a logfile called ConsoleOutput.txt in every module being tested
-        var file = new File(System.getProperty("user.dir"), "ConsoleOutput.txt");
-        FileAppender fa = FileAppender.newBuilder()
-                .setName("fileAppender")
-                .withAppend(true)
-                .withFileName(file.toString())
-                .setConfiguration(cfg)
-                .build();
-        cfg.addAppender(fa);
-
-        Configurator.initialize(cfg);
+        Configurator.initialize(new DefaultConfiguration());
         Configurator.setRootLevel(Level.DEBUG);
+        // silence the apache wire log output, it's too much to handle in CI
+        Configurator.setAllLevels("org.apache.http.wire", Level.INFO);
     }
 }
