d4ac1e642370800e8081173da81f494795952095
Lukas Deichmann <lukasdeichmann+gitlab@gmail.com>
2020-01-27 12:02:40 +0100

    Log test output per package for tests using the TestLogging utility

--- test/src/main/java/test/org/somda/common/TestLogging.java
+++ test/src/main/java/test/org/somda/common/TestLogging.java
@@ -1,8 +1,11 @@
 package test.org.somda.common;
 
 import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.appender.FileAppender;
 import org.apache.logging.log4j.core.config.Configurator;
 import org.apache.logging.log4j.core.config.DefaultConfiguration;
+
+import java.io.File;
 
 /**
  * Utility class with static logging configuration for tests.
@@ -11,8 +14,21 @@ public class TestLogging {
     /**
      * Configures a default logging valid for all tests.
      */
+
     public static void configure() {
-        Configurator.initialize(new DefaultConfiguration());
+        var cfg = new DefaultConfiguration();
+
+        // this will create a logfile called ConsoleOutput.txt in every module being tested
+        var file = new File(System.getProperty("user.dir"), "ConsoleOutput.txt");
+        FileAppender fa = FileAppender.newBuilder()
+                .setName("fileAppender")
+                .withAppend(true)
+                .withFileName(file.toString())
+                .setConfiguration(cfg)
+                .build();
+        cfg.addAppender(fa);
+
+        Configurator.initialize(cfg);
         Configurator.setRootLevel(Level.DEBUG);
     }
 }
