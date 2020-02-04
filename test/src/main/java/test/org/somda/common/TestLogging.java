a9ccc5c5ecf33123523fb88008ba2e8a8a052b79
Lukas Deichmann <lukasdeichmann+gitlab@gmail.com>
2020-02-04 16:13:57 +0100

    #77 Make jetty more quiet, or at least try to.

--- test/src/main/java/test/org/somda/common/TestLogging.java
+++ test/src/main/java/test/org/somda/common/TestLogging.java
@@ -15,8 +15,9 @@ public class TestLogging {
     public static void configure() {
         Configurator.initialize(new DefaultConfiguration());
         Configurator.setRootLevel(Level.DEBUG);
-        // silence the apache httpclient log output a little, it's too much to handle in CI
+        // silence the apache httpclient and jetty log output a little, it's too much to handle in CI
         Configurator.setAllLevels("org.apache.http.wire", Level.INFO);
         Configurator.setAllLevels("org.apache.http.headers", Level.INFO);
+        Configurator.setAllLevels("org.eclipse.jetty", Level.INFO);
     }
 }
