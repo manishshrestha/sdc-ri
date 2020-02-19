8a22355135c36e4910e604fddeb3b2fddbb0c175
David Gregorczyk <david.gregorczyk@web.de>
2020-02-19 18:33:13 +0100

    #54 Fix bug: add contexts on context state update if new.

--- biceps/src/main/java/org/somda/sdc/biceps/common/storage/MdibStorageImpl.java
+++ biceps/src/main/java/org/somda/sdc/biceps/common/storage/MdibStorageImpl.java
@@ -2,14 +2,14 @@ package org.somda.sdc.biceps.common.storage;
 
 import com.google.inject.assistedinject.Assisted;
 import com.google.inject.assistedinject.AssistedInject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.somda.sdc.biceps.common.*;
 import org.somda.sdc.biceps.common.access.WriteDescriptionResult;
 import org.somda.sdc.biceps.common.access.WriteStateResult;
 import org.somda.sdc.biceps.common.factory.MdibEntityFactory;
 import org.somda.sdc.biceps.common.storage.helper.MdibStorageUtil;
 import org.somda.sdc.biceps.model.participant.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
 import java.math.BigInteger;
@@ -275,7 +275,7 @@ public class MdibStorageImpl implements MdibStorage {
         if (mdibEntity.getDescriptor() instanceof AbstractContextDescriptor) {
             contextStates.putAll(mdibEntity.getStates().stream()
                     .map(state -> (AbstractContextState) state)
-                    .collect(Collectors.toMap(state -> state.getHandle(), state -> state)));
+                    .collect(Collectors.toMap(AbstractMultiState::getHandle, state -> state)));
         }
 
         insertedEntities.add(mdibEntity);
@@ -305,7 +305,6 @@ public class MdibStorageImpl implements MdibStorage {
                 try {
                     descr = typeValidator.resolveDescriptorType(modification.getClass())
                             .getConstructor().newInstance();
-
                 } catch (Exception e) {
                     LOG.warn(String.format("Ignore modification. Reason: could not instantiate descriptor type for handle %s",
                             modification.getDescriptorHandle()), e);
@@ -320,7 +319,7 @@ public class MdibStorageImpl implements MdibStorage {
                                 entities.put(mdibEntity.getHandle(),
                                         entityFactory.replaceStates(mdibEntity, Collections.singletonList(modification))))
                         .orElse(states -> {
-                            final List<AbstractState> newStates = new ArrayList<>();
+                            final List<AbstractMultiState> newStates = new ArrayList<>();
                             typeValidator.toMultiState(modification).ifPresent(modifiedMultiState ->
                             {
                                 AtomicBoolean found = new AtomicBoolean(false);
@@ -340,6 +339,12 @@ public class MdibStorageImpl implements MdibStorage {
                             });
                             entities.put(mdibEntity.getHandle(), entityFactory.replaceStates(mdibEntity,
                                     Collections.unmodifiableList(newStates)));
+                            newStates.stream()
+                                    .filter(abstractMultiState -> abstractMultiState instanceof AbstractContextState)
+                                    .map(abstractMultiState -> (AbstractContextState) abstractMultiState)
+                                    .collect(Collectors.toList())
+                                    .forEach(abstractContextState ->
+                                            contextStates.put(abstractContextState.getHandle(), abstractContextState));
                         });
             }
         }
