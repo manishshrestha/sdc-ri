827f0486bdd4c05a4df8a27d9481dd6351a8efc6
Maximilian Pilz <maximilian.pilz.git@gmail.com>
2020-02-27 17:25:21 +0100

    #89 add space

--- glue/src/main/java/org/somda/sdc/glue/provider/plugin/SdcRequiredTypesAndScopes.java
+++ glue/src/main/java/org/somda/sdc/glue/provider/plugin/SdcRequiredTypesAndScopes.java
@@ -161,7 +161,7 @@ public class SdcRequiredTypesAndScopes implements SdcDevicePlugin, MdibAccessObs
                 uris.add(ComplexDeviceComponentMapper.fromComplexDeviceComponent(mdsDescriptor));
             } catch (UriMapperGenerationArgumentException e) {
                 LOG.warn("The URI generation based on the given MdsDescriptor with the handle " +
-                        mdsDescriptor.getHandle() + "failed", e);
+                        mdsDescriptor.getHandle() + " failed", e);
             }
         }
 
