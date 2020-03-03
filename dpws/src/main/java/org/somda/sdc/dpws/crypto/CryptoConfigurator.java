5dd1df68722236f739b633589a4495c7fc8a38ed
David Gregorczyk <david.gregorczyk@web.de>
2020-03-03 16:44:30 +0100

    #120 Throw exception in case key or trust store could not be pulled from crypto settings.

--- dpws/src/main/java/org/somda/sdc/dpws/crypto/CryptoConfigurator.java
+++ dpws/src/main/java/org/somda/sdc/dpws/crypto/CryptoConfigurator.java
@@ -44,6 +44,8 @@ public class CryptoConfigurator {
             KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
             ks.load(cryptoSettings.getKeyStoreStream().get(), cryptoSettings.getKeyStorePassword().toCharArray());
             sslContextBuilder.loadKeyMaterial(ks, cryptoSettings.getKeyStorePassword().toCharArray());
+        } else {
+            throw new IOException("Expected key store, but none found");
         }
 
         // trust store
@@ -57,6 +59,8 @@ public class CryptoConfigurator {
             KeyStore ts = KeyStore.getInstance(KeyStore.getDefaultType());
             ts.load(cryptoSettings.getTrustStoreStream().get(), cryptoSettings.getTrustStorePassword().toCharArray());
             sslContextBuilder.loadTrustMaterial(ts, null);
+        } else {
+            throw new IOException("Expected trust store, but none found");
         }
 
         return sslContextBuilder.build();
