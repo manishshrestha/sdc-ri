57e07b1c1a2da959b02780b03b8beb3c98560ad1
Lukas Deichmann <lukasdeichmann@gmail.com>
2020-03-18 12:36:02 +0100

    #132 move biceps and sdc prefixes into separate constants

--- glue/src/main/java/org/somda/sdc/glue/common/CommonConstants.java
+++ glue/src/main/java/org/somda/sdc/glue/common/CommonConstants.java
@@ -7,6 +7,12 @@ public class CommonConstants {
     public static final String NAMESPACE_SDC = "http://standards.ieee.org/downloads/11073/11073-20701-2018";
 
     /**
+     * Prefix used for the SDC namespace.
+     */
+    public static final String NAMESPACE_SDC_PREFIX = "sdc";
+
+
+    /**
      * Namespace mappings of the MDPWS model.
      * <p>
      * todo DGr populate MDPWS namespace mappings constant
@@ -16,12 +22,16 @@ public class CommonConstants {
     /**
      * Namespace mappings of the BICEPS model.
      */
-    public static final String NAMESPACE_PREFIX_MAPPINGS_BICEPS = "{ext:" + org.somda.sdc.biceps.common.CommonConstants.NAMESPACE_EXTENSION + "}" +
-            "{pm:" + org.somda.sdc.biceps.common.CommonConstants.NAMESPACE_PARTICIPANT + "}" +
-            "{msg:" + org.somda.sdc.biceps.common.CommonConstants.NAMESPACE_MESSAGE + "}";
+    public static final String NAMESPACE_PREFIX_MAPPINGS_BICEPS =
+            "{" + org.somda.sdc.biceps.common.CommonConstants.NAMESPACE_EXTENSION_PREFIX + ":"
+                    + org.somda.sdc.biceps.common.CommonConstants.NAMESPACE_EXTENSION + "}" +
+                    "{" + org.somda.sdc.biceps.common.CommonConstants.NAMESPACE_PARTICIPANT_PREFIX + ":"
+                    + org.somda.sdc.biceps.common.CommonConstants.NAMESPACE_PARTICIPANT + "}" +
+                    "{" + org.somda.sdc.biceps.common.CommonConstants.NAMESPACE_MESSAGE_PREFIX + ":"
+                    + org.somda.sdc.biceps.common.CommonConstants.NAMESPACE_MESSAGE + "}";
 
     /**
      * Namespace mappings of the SDC Glue model.
      */
-    public static final String NAMESPACE_PREFIX_MAPPINGS_GLUE = "{sdc:" + NAMESPACE_SDC + "}";
+    public static final String NAMESPACE_PREFIX_MAPPINGS_GLUE = "{" + NAMESPACE_SDC_PREFIX + ":" + NAMESPACE_SDC + "}";
 }
