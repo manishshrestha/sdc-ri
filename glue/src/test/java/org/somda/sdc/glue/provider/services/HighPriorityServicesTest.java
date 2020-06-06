69610589d986fe0c98f91b522b84eb3a90caa16c
David Gregorczyk <david.gregorczyk@web.de>
2020-06-06 11:17:31 +0200

    #82 Test set service commands including safety information processing and MustUnderstandAppender.

--- glue/src/test/java/org/somda/sdc/glue/provider/services/HighPriorityServicesTest.java
+++ glue/src/test/java/org/somda/sdc/glue/provider/services/HighPriorityServicesTest.java
@@ -10,12 +10,8 @@ import org.somda.sdc.biceps.common.access.MdibAccess;
 import org.somda.sdc.biceps.common.storage.PreprocessingException;
 import org.somda.sdc.biceps.model.message.Activate;
 import org.somda.sdc.biceps.model.message.ActivateResponse;
-import org.somda.sdc.biceps.model.message.GetContainmentTree;
-import org.somda.sdc.biceps.model.message.GetContainmentTreeResponse;
 import org.somda.sdc.biceps.model.message.GetContextStates;
 import org.somda.sdc.biceps.model.message.GetContextStatesResponse;
-import org.somda.sdc.biceps.model.message.GetDescriptor;
-import org.somda.sdc.biceps.model.message.GetDescriptorResponse;
 import org.somda.sdc.biceps.model.message.GetMdDescription;
 import org.somda.sdc.biceps.model.message.GetMdDescriptionResponse;
 import org.somda.sdc.biceps.model.message.GetMdState;
@@ -23,6 +19,26 @@ import org.somda.sdc.biceps.model.message.GetMdStateResponse;
 import org.somda.sdc.biceps.model.message.GetMdib;
 import org.somda.sdc.biceps.model.message.GetMdibResponse;
 import org.somda.sdc.biceps.model.message.InvocationState;
+import org.somda.sdc.biceps.model.message.SetAlertState;
+import org.somda.sdc.biceps.model.message.SetAlertStateResponse;
+import org.somda.sdc.biceps.model.message.SetComponentState;
+import org.somda.sdc.biceps.model.message.SetComponentStateResponse;
+import org.somda.sdc.biceps.model.message.SetContextState;
+import org.somda.sdc.biceps.model.message.SetContextStateResponse;
+import org.somda.sdc.biceps.model.message.SetMetricState;
+import org.somda.sdc.biceps.model.message.SetMetricStateResponse;
+import org.somda.sdc.biceps.model.message.SetString;
+import org.somda.sdc.biceps.model.message.SetStringResponse;
+import org.somda.sdc.biceps.model.message.SetValue;
+import org.somda.sdc.biceps.model.message.SetValueResponse;
+import org.somda.sdc.biceps.model.participant.AbstractContextState;
+import org.somda.sdc.biceps.model.participant.AbstractDeviceComponentState;
+import org.somda.sdc.biceps.model.participant.AbstractMetricState;
+import org.somda.sdc.biceps.model.participant.ChannelState;
+import org.somda.sdc.biceps.model.participant.ComponentActivation;
+import org.somda.sdc.biceps.model.participant.ContextAssociation;
+import org.somda.sdc.biceps.model.participant.LimitAlertConditionState;
+import org.somda.sdc.biceps.model.participant.NumericMetricState;
 import org.somda.sdc.biceps.model.participant.PatientContextState;
 import org.somda.sdc.biceps.provider.access.LocalMdibAccess;
 import org.somda.sdc.biceps.provider.access.factory.LocalMdibAccessFactory;
@@ -35,23 +51,29 @@ import org.somda.sdc.dpws.soap.SoapUtil;
 import org.somda.sdc.dpws.soap.exception.SoapFaultException;
 import org.somda.sdc.dpws.soap.interception.RequestResponseObject;
 import org.somda.sdc.glue.UnitTestUtil;
+import org.somda.sdc.glue.common.MustUnderstandAppender;
 import org.somda.sdc.glue.provider.sco.Context;
 import org.somda.sdc.glue.provider.sco.IncomingSetServiceRequest;
 import org.somda.sdc.glue.provider.sco.InvocationResponse;
 import org.somda.sdc.glue.provider.sco.OperationInvocationReceiver;
 import org.somda.sdc.glue.provider.services.factory.ServicesFactory;
+import org.somda.sdc.mdpws.consumer.safety.SafetyInfoBuilder;
+import org.somda.sdc.mdpws.model.SafetyInfoType;
 import org.somda.sdc.mdpws.provider.safety.SafetyXPath;
 
-import javax.xml.namespace.QName;
+import javax.xml.bind.JAXBElement;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 
@@ -258,7 +280,6 @@ class HighPriorityServicesTest {
     void getContextStates() throws SoapFaultException {
         {
             // Check with empty filter
-
             var rrObj = createRequestResponseObject(new GetContextStates());
             highPriorityServices.getContextStates(rrObj);
 
@@ -320,9 +341,101 @@ class HighPriorityServicesTest {
     }
 
     @Test
-    void setValue() throws SoapFaultException {
+    void setValuePlusSafetyInformation() throws SoapFaultException {
         var called = new AtomicBoolean(false);
-        var expectedArgs = List.of(mock(Activate.Argument.class), mock(Activate.Argument.class));
+        var expectedValue = BigDecimal.ONE;
+        var expectedDualChannels = Map.of("dc1", "32d22ac3423bdeef1b99311530cd4ad753f0eff7",
+                "dc2", "453496f7220805b888b375a34635a3f9c1226cc9");
+        var expectedSafetyContexts = Map.of("sc1", "Test", "sc2", BigDecimal.TEN);
+        highPriorityServices.addOperationInvocationReceiver(new OperationInvocationReceiver() {
+            @IncomingSetServiceRequest(operationHandle = Handles.OPERATION_0)
+            InvocationResponse process(Context context, BigDecimal actualValue) {
+                called.set(true);
+                assertEquals(expectedValue, actualValue);
+                assertEquals(2, context.getDualChannelValues().size());
+                assertEquals("dc1", context.getDualChannelValues().get("dc1")
+                        .getReferencedSelector());
+                assertEquals(expectedDualChannels.get("dc1"), context.getDualChannelValues().get("dc1")
+                        .getValue());
+                assertEquals("dc2", context.getDualChannelValues().get("dc2")
+                        .getReferencedSelector());
+                assertEquals(expectedDualChannels.get("dc2"), context.getDualChannelValues().get("dc2")
+                        .getValue());
+
+                assertEquals(2, context.getSafetyContextValues().size());
+                assertEquals("sc1", context.getSafetyContextValues().get("sc1")
+                        .getReferencedSelector());
+                assertEquals(expectedSafetyContexts.get("sc1"), context.getSafetyContextValues().get("sc1")
+                        .getValue());
+                assertEquals("sc2", context.getSafetyContextValues().get("sc2")
+                        .getReferencedSelector());
+                assertEquals(expectedSafetyContexts.get("sc2"), context.getSafetyContextValues().get("sc2")
+                        .getValue());
+
+                return context.createSuccessfulResponse(InvocationState.FIN);
+            }
+        });
+
+        var request = new SetValue();
+        request.setRequestedNumericValue(expectedValue);
+        request.setOperationHandleRef(Handles.OPERATION_0);
+        var rrObj = createRequestResponseObject(request);
+
+        var builder = SafetyInfoBuilder.create();
+        builder.addDualChannelValue("dc1", expectedDualChannels.get("dc1"));
+        builder.addDualChannelValue("dc2", expectedSafetyContexts.get("dc2"));
+        builder.addSafetyContextValue("sc1", expectedSafetyContexts.get("sc1"));
+        builder.addSafetyContextValue("sc2", expectedSafetyContexts.get("sc2"));
+
+        assertDoesNotThrow(() -> rrObj.getRequest().getOriginalEnvelope().getHeader().getAny()
+                .add(MustUnderstandAppender.append(builder.get(), true)));
+
+        // Check if must understand has been attached
+        var headers = rrObj.getRequest().getOriginalEnvelope().getHeader().getAny();
+        var safetyInfoElement = (JAXBElement<SafetyInfoType>) headers.get(headers.size() - 1);
+        assertEquals("true",
+                safetyInfoElement.getValue().getOtherAttributes().get(CommonConstants.QNAME_MUST_UNDERSTAND_ATTRIBUTE));
+
+        highPriorityServices.setValue(rrObj);
+
+        var actualResponse = soapUtil.getBody(rrObj.getResponse(), SetValueResponse.class);
+        assertTrue(actualResponse.isPresent());
+        assertTrue(called.get());
+    }
+
+    @Test
+    void setString() throws SoapFaultException {
+        var called = new AtomicBoolean(false);
+        var expectedValue = "aString";
+        highPriorityServices.addOperationInvocationReceiver(new OperationInvocationReceiver() {
+            @IncomingSetServiceRequest(operationHandle = Handles.OPERATION_0)
+            InvocationResponse process(Context context, String actualValue) {
+                called.set(true);
+                assertEquals(expectedValue, actualValue);
+                return context.createSuccessfulResponse(InvocationState.FIN);
+            }
+        });
+
+        var request = new SetString();
+        request.setRequestedStringValue(expectedValue);
+        request.setOperationHandleRef(Handles.OPERATION_0);
+        var rrObj = createRequestResponseObject(request);
+
+        highPriorityServices.setString(rrObj);
+
+        var actualResponse = soapUtil.getBody(rrObj.getResponse(), SetStringResponse.class);
+        assertTrue(actualResponse.isPresent());
+        assertTrue(called.get());
+    }
+
+    @Test
+    void activate() throws SoapFaultException {
+        var called = new AtomicBoolean(false);
+        var expectedArgs = List.of("A", "B", "C").stream().map(s -> {
+            var argument = new Activate.Argument();
+            argument.setArgValue(s);
+            return argument;
+        }).collect(Collectors.toList());
         highPriorityServices.addOperationInvocationReceiver(new OperationInvocationReceiver() {
             @IncomingSetServiceRequest(operationHandle = Handles.OPERATION_0, listType = Activate.Argument.class)
             InvocationResponse process(Context context, List<Activate.Argument> actualArgs) {
@@ -333,20 +446,9 @@ class HighPriorityServicesTest {
         });
 
         var request = new Activate();
-        request.getArgument().addAll(expectedArgs);
+        request.setArgument(expectedArgs);
         request.setOperationHandleRef(Handles.OPERATION_0);
         var rrObj = createRequestResponseObject(request);
-
-//        var safetyInfo = new SafetyInfoType();
-//        DualChannelValueType v;
-//        safetyInfo.getDualChannel().getValue().add()
-//        ObjectFactory factory = new ObjectFactory();
-//        factory.
-//        safetyInfo.setDualChannel(new DualChannelType());
-//        safetyInfo.setDualChannel(new DualChannelType());
-
-//        rrObj.getRequest().getOriginalEnvelope().getHeader().getAny()
-//                .add()
 
         highPriorityServices.activate(rrObj);
 
@@ -356,28 +458,116 @@ class HighPriorityServicesTest {
     }
 
     @Test
-    void setString() {
+    void setComponentState() throws SoapFaultException {
+        var called = new AtomicBoolean(false);
+        AbstractDeviceComponentState state = new ChannelState();
+        state.setActivationState(ComponentActivation.FAIL);
+        var expectedStates = List.of(state);
+        highPriorityServices.addOperationInvocationReceiver(new OperationInvocationReceiver() {
+            @IncomingSetServiceRequest(operationHandle = Handles.OPERATION_0, listType =
+                    AbstractDeviceComponentState.class)
+            InvocationResponse process(Context context, List<AbstractDeviceComponentState> actualStates) {
+                called.set(true);
+                assertEquals(expectedStates.get(0).getActivationState(), actualStates.get(0).getActivationState());
+                return context.createSuccessfulResponse(InvocationState.FIN);
+            }
+        });
+
+        var request = new SetComponentState();
+        request.setProposedComponentState(expectedStates);
+        request.setOperationHandleRef(Handles.OPERATION_0);
+        var rrObj = createRequestResponseObject(request);
+
+        highPriorityServices.setComponentState(rrObj);
+
+        var actualResponse = soapUtil.getBody(rrObj.getResponse(), SetComponentStateResponse.class);
+        assertTrue(actualResponse.isPresent());
+        assertTrue(called.get());
     }
 
     @Test
-    void activate() {
+    void setContextState() throws SoapFaultException {
+        var called = new AtomicBoolean(false);
+        AbstractContextState state = new PatientContextState();
+        state.setContextAssociation(ContextAssociation.DIS);
+        var expectedStates = List.of(state);
+        highPriorityServices.addOperationInvocationReceiver(new OperationInvocationReceiver() {
+            @IncomingSetServiceRequest(operationHandle = Handles.OPERATION_0, listType =
+                    AbstractContextState.class)
+            InvocationResponse process(Context context, List<AbstractContextState> actualStates) {
+                called.set(true);
+                assertEquals(expectedStates.get(0).getContextAssociation(),
+                        actualStates.get(0).getContextAssociation());
+                return context.createSuccessfulResponse(InvocationState.FIN);
+            }
+        });
+
+        var request = new SetContextState();
+        request.setProposedContextState(expectedStates);
+        request.setOperationHandleRef(Handles.OPERATION_0);
+        var rrObj = createRequestResponseObject(request);
+
+        highPriorityServices.setContextState(rrObj);
+
+        var actualResponse = soapUtil.getBody(rrObj.getResponse(), SetContextStateResponse.class);
+        assertTrue(actualResponse.isPresent());
+        assertTrue(called.get());
     }
 
     @Test
-    void setComponentState() {
+    void setAlertState() throws SoapFaultException {
+        var called = new AtomicBoolean(false);
+        var expectedState = new LimitAlertConditionState();
+        highPriorityServices.addOperationInvocationReceiver(new OperationInvocationReceiver() {
+            @IncomingSetServiceRequest(operationHandle = Handles.OPERATION_0)
+            InvocationResponse process(Context context, LimitAlertConditionState actualStates) {
+                called.set(true);
+                assertEquals(expectedState.getActivationState(), actualStates.getActivationState());
+                return context.createSuccessfulResponse(InvocationState.FIN);
+            }
+        });
+
+        var request = new SetAlertState();
+        request.setProposedAlertState(expectedState);
+        request.setOperationHandleRef(Handles.OPERATION_0);
+        var rrObj = createRequestResponseObject(request);
+
+        highPriorityServices.setAlertState(rrObj);
+
+        var actualResponse = soapUtil.getBody(rrObj.getResponse(), SetAlertStateResponse.class);
+        assertTrue(actualResponse.isPresent());
+        assertTrue(called.get());
     }
 
     @Test
-    void setContextState() {
+    void setMetricState() throws SoapFaultException {
+        var called = new AtomicBoolean(false);
+        AbstractMetricState state = new NumericMetricState();
+        state.setStateVersion(BigInteger.TEN);
+        var expectedStates = List.of(state);
+        highPriorityServices.addOperationInvocationReceiver(new OperationInvocationReceiver() {
+            @IncomingSetServiceRequest(operationHandle = Handles.OPERATION_0, listType =
+                    AbstractMetricState.class)
+            InvocationResponse process(Context context, List<AbstractMetricState> actualStates) {
+                called.set(true);
+                assertEquals(expectedStates.get(0).getStateVersion(),
+                        actualStates.get(0).getStateVersion());
+                return context.createSuccessfulResponse(InvocationState.FIN);
+            }
+        });
+
+        var request = new SetMetricState();
+        request.setProposedMetricState(expectedStates);
+        request.setOperationHandleRef(Handles.OPERATION_0);
+        var rrObj = createRequestResponseObject(request);
+
+        highPriorityServices.setMetricState(rrObj);
+
+        var actualResponse = soapUtil.getBody(rrObj.getResponse(), SetMetricStateResponse.class);
+        assertTrue(actualResponse.isPresent());
+        assertTrue(called.get());
     }
 
-    @Test
-    void setAlertState() {
-    }
-
-    @Test
-    void setMetricState() {
-    }
 
     private int getRecursiveStateCount(MdibAccess mdibAccess, MdibEntity entity) {
         int sum = entity.getStates().size();
