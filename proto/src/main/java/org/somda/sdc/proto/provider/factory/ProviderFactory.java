7fd6189162860eb240f44d04476ce8a3171db499
Lukas Deichmann <lukasdeichmann+gitlab@gmail.com>
2020-08-03 09:29:48 +0200

    Remove leftover javadoc in ProviderFactory

--- proto/src/main/java/org/somda/sdc/proto/provider/factory/ProviderFactory.java
+++ proto/src/main/java/org/somda/sdc/proto/provider/factory/ProviderFactory.java
@@ -16,18 +16,8 @@ public interface ProviderFactory {
      *
      * @param eprAddress                   the unique and persisted endpoint reference (EPR) of the device
      * @param providerSettings             settings, e.g. the network interface the device shall bind to
-     * @param mdibAccess                   the MDIB to be exposed on the network
-     * @param operationInvocationReceivers callback interceptors for incoming set service requests.
-     * @param plugins                      {@link org.somda.sdc.glue.provider.plugin.SdcRequiredTypesAndScopes} if the
-     *                                     collection is empty or custom plugins to run on start up and shut down in the
-     *                                     order given by this collection.
-     *                                     Make sure plugins are independent to each other.
-     *                                     Also make sure that at least the
-     *                                     {@link org.somda.sdc.glue.provider.plugin.SdcRequiredTypesAndScopes}
-     *                                     or a functionally equivalent plugin is loaded.
-     *                                     See {@link SdcDevicePlugin} for more details.
-     * @return a new {@link SdcDevice}.
-     * Use {@link SdcDevice#startAsync()} in order to start exposing the device on the network.
+     * @return a new {@link Provider}.
+     * Use {@link Provider#startAsync()} in order to start exposing the device on the network.
      */
     Provider create(@Assisted String eprAddress,
                     @Assisted ProviderSettings providerSettings);
