0385713c4ec75a342ed41ae4716f68814fdf7c09
grebemal <malte.grebe.contractor@draeger.com>
2020-09-22 15:01:56 +0200

    #190 - fixed regex, changed unittests

--- dpws/src/main/java/org/somda/sdc/dpws/DpwsConstants.java
+++ dpws/src/main/java/org/somda/sdc/dpws/DpwsConstants.java
@@ -188,26 +188,26 @@ public class DpwsConstants {
     private static final String SEGMENT = P_CHAR + "*(;" + PARAM + ")*";
     private static final String PATH_SEGMENTS = SEGMENT + "(/" + SEGMENT + ")*";
 
-    private static final String ESCAPED = "%" + HEXDIG + HEXDIG;
+    private static final String ESCAPED = "(%" + HEXDIG + HEXDIG + ")";
     private static final String RESERVED = "[;/?:@&=+$,]";
     private static final String UNRESERVED = "[a-zA-Z0-9\\-_.!~*'()]";
     private static final String REL_SEGMENT = "(" + UNRESERVED + "|" + ESCAPED + "|[;@&=+$,])+";
 
     private static final String ABS_PATH = "({=path}/" + PATH_SEGMENTS + ")";
-    private static final String NET_PATH = "//({=authority}" + AUTHORITY + ")(" + ABS_PATH + ")?";
-    private static final String REL_PATH = REL_SEGMENT + "(" + ABS_PATH + ")?";
+    private static final String NET_PATH = "({=netPath}//({=authority}" + AUTHORITY + ")(" + ABS_PATH + ")?)";
+    private static final String REL_PATH = "({relPath}" + REL_SEGMENT + "(" + ABS_PATH + ")?)";
 
-    public static final String RELATIVE_URI = "(" + NET_PATH + "|" + ABS_PATH + "|" + REL_PATH + ")(\\?({=query}" + QUERY + "))?";
+    public static final String RELATIVE_URI = "({relativeUri}(" + NET_PATH + "|" + ABS_PATH + "|" + REL_PATH + ")(\\?({=relativeUriQuery}" + QUERY + "))?)";
 
-    private static final String URIC = RESERVED + "|" + UNRESERVED + "|" + ESCAPED;
-    private static final String URIC_NO_SLASH = UNRESERVED + "|" + ESCAPED + "|" + "[;?:@&=+$,]";
-    private static final String OPAQUE_PART = URIC_NO_SLASH + URIC + "*";
-    private static final String HIER_PART = "(" + NET_PATH + "|" + ABS_PATH + ")(\\?({=query}" + QUERY + "))?";
+    private static final String URIC = "(" + RESERVED + "|" + UNRESERVED + "|" + ESCAPED + ")";
+    private static final String URIC_NO_SLASH = "(" + UNRESERVED + "|" + ESCAPED + "|" + "[;?:@&=+$,])";
+    private static final String OPAQUE_PART = "({opaquePart}(" + URIC_NO_SLASH + URIC + ")*)";
+    private static final String HIER_PART = "({hierPart}(" + NET_PATH + "|" + ABS_PATH + ")(\\?({=absoluteUriQuery}" + QUERY + "))?)";
 
-    public static final String ABSOLUTE_URI = "({scheme}" + SCHEME_SEGMENT + ")" +
-            ":" + "(" + HIER_PART + "|" + OPAQUE_PART + ")";
+    public static final String ABSOLUTE_URI = "({absoluteUri}({scheme}" + SCHEME_SEGMENT + ")" +
+            ":" + "(" + HIER_PART + "|" + OPAQUE_PART + "))";
 
-    public static final String URI_REFERENCE = "(({absoluteUri}" + ABSOLUTE_URI + ")|({relativeUri}" + RELATIVE_URI + "))(#({fragment}" + FRAGMENT + "))?";
+    public static final String URI_REFERENCE = "(" + ABSOLUTE_URI + "|" + RELATIVE_URI + ")?(#({fragment}" + FRAGMENT + "))?";
 
     // Added negative lookahead for "//" to prevent authority from being interpreted as path
     public static final String URI_REGEX = "^(" +
--- dpws/src/test/java/org/somda/sdc/dpws/RegexTest.java
+++ dpws/src/test/java/org/somda/sdc/dpws/RegexTest.java
@@ -8,6 +8,7 @@ import test.org.somda.common.LoggingTestWatcher;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(LoggingTestWatcher.class)
@@ -17,8 +18,8 @@ public class RegexTest {
     private static final Pattern URI_PATTERN = new Pattern(DpwsConstants.URI_REGEX);
     private static final Pattern SEGMENT_PATTERN = new Pattern(DpwsConstants.AUTHORITY);
     private static final Pattern URI_REFERENCE_PATTERN = new Pattern(DpwsConstants.URI_REFERENCE);
-    private static final Pattern RELATIVE_URI = new Pattern(DpwsConstants.RELATIVE_URI);
-    private static final Pattern ABSOLUTE_URI = new Pattern(DpwsConstants.ABSOLUTE_URI);
+    private static final Pattern RELATIVE_URI_PATTERN = new Pattern(DpwsConstants.RELATIVE_URI);
+    private static final Pattern ABSOLUTE_URI_PATTERN = new Pattern(DpwsConstants.ABSOLUTE_URI);
 
     @Test
     void segment() {
@@ -111,16 +112,30 @@ public class RegexTest {
             assertEquals("123", matcher.group("port"));
             assertEquals("%C3%A4", matcher.group("host"));
             assertEquals("/path", matcher.group("path"));
-            assertEquals("query?query", matcher.group("query"));
+            assertEquals("query?query", matcher.group("absoluteUriQuery"));
             assertEquals("fragment", matcher.group("fragment"));
+            assertEquals("//user@%C3%A4:123/path?query?query", matcher.group("hierPart"));
+            assertNull(matcher.group("relativeUri"));
+            assertNull(matcher.group("opaquePart"));
+        }
+        {
+            Matcher matcher = URI_REFERENCE_PATTERN.matcher("scheme:aaahhh??");
+            assertTrue(matcher.matches());
+            assertEquals("scheme:aaahhh??", matcher.group("absoluteUri"));
+            assertEquals("aaahhh??", matcher.group("opaquePart"));
+            assertNull(matcher.group("relativeUri"));
+            assertNull(matcher.group("hierPart"));
         }
         {
             Matcher matcher = URI_REFERENCE_PATTERN.matcher("/path?query?query#fragment");
             assertTrue(matcher.matches());
             assertEquals("/path?query?query", matcher.group("relativeUri"));
             assertEquals("/path", matcher.group("path"));
-            assertEquals("query?query", matcher.group("query"));
+            assertEquals("query?query", matcher.group("relativeUriQuery"));
             assertEquals("fragment", matcher.group("fragment"));
+            assertNull(matcher.group("absoluteUri"));
+            assertNull(matcher.group("netPath"));
+            assertNull(matcher.group("relPath"));
         }
         {
             Matcher matcher = URI_REFERENCE_PATTERN.matcher("//user@%C3%A4:123/path?query?query#fragment");
@@ -130,40 +145,64 @@ public class RegexTest {
             assertEquals("123", matcher.group("port"));
             assertEquals("%C3%A4", matcher.group("host"));
             assertEquals("/path", matcher.group("path"));
-            assertEquals("query?query", matcher.group("query"));
+            assertEquals("query?query", matcher.group("relativeUriQuery"));
             assertEquals("fragment", matcher.group("fragment"));
+            assertNull(matcher.group("absoluteUri"));
+            assertNull(matcher.group("relPath"));
         }
     }
 
     @Test
     void absoluteUri() {
         {
-            Matcher matcher = ABSOLUTE_URI.matcher("scheme://user@%C3%A4:123/path?query?query");
+            Matcher matcher = ABSOLUTE_URI_PATTERN.matcher("scheme://user@%C3%A4:123/path?query?query");
             assertTrue(matcher.matches());
+            assertEquals("scheme://user@%C3%A4:123/path?query?query", matcher.group("absoluteUri"));
             assertEquals("user", matcher.group("userInfo"));
             assertEquals("123", matcher.group("port"));
             assertEquals("%C3%A4", matcher.group("host"));
             assertEquals("/path", matcher.group("path"));
-            assertEquals("query?query", matcher.group("query"));
+            assertEquals("query?query", matcher.group("absoluteUriQuery"));
+            assertNull(matcher.group("opaquePart"));
+        }
+        {
+            Matcher matcher = ABSOLUTE_URI_PATTERN.matcher("scheme:aaahhh??");
+            assertTrue(matcher.matches());
+            assertEquals("scheme:aaahhh??", matcher.group("absoluteUri"));
+            assertEquals("aaahhh??", matcher.group("opaquePart"));
+            assertNull(matcher.group("hierPart"));
         }
     }
 
     @Test
     void relativeUri() {
         {
-            Matcher matcher = RELATIVE_URI.matcher("/path?query?query");
+            Matcher matcher = RELATIVE_URI_PATTERN.matcher("/path?query?query");
             assertTrue(matcher.matches());
+            assertEquals("/path?query?query", matcher.group("relativeUri"));
             assertEquals("/path", matcher.group("path"));
-            assertEquals("query?query", matcher.group("query"));
+            assertEquals("query?query", matcher.group("relativeUriQuery"));
+            assertNull(matcher.group("authority"));
+            assertNull(matcher.group("relPath"));
         }
         {
-            Matcher matcher = RELATIVE_URI.matcher("//user@%C3%A4:123/path?query?query");
+            Matcher matcher = RELATIVE_URI_PATTERN.matcher("//user@%C3%A4:123/path?query?query");
             assertTrue(matcher.matches());
+            assertEquals("//user@%C3%A4:123/path?query?query", matcher.group("relativeUri"));
             assertEquals("user", matcher.group("userInfo"));
             assertEquals("123", matcher.group("port"));
             assertEquals("%C3%A4", matcher.group("host"));
             assertEquals("/path", matcher.group("path"));
-            assertEquals("query?query", matcher.group("query"));
+            assertEquals("query?query", matcher.group("relativeUriQuery"));
+            assertNull(matcher.group("relPath"));
+        }
+        {
+            Matcher matcher = RELATIVE_URI_PATTERN.matcher("*");
+            assertTrue(matcher.matches());
+            assertEquals("*", matcher.group("relativeUri"));
+            assertEquals("*", matcher.group("relPath"));
+            assertNull(matcher.group("path"));
+            assertNull(matcher.group("authority"));
         }
     }
 }
