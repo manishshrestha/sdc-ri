1fbf1dc288b143b980988c072bf0d505206bebf4
Malte Grebe <malte.grebe@gmx.de>
2020-10-20 09:32:57 +0200

    #198 - changed javadoc

--- biceps/src/main/java/org/somda/sdc/biceps/common/preprocessing/PreprocessingInjectorWrapper.java
+++ biceps/src/main/java/org/somda/sdc/biceps/common/preprocessing/PreprocessingInjectorWrapper.java
@@ -4,7 +4,8 @@ import com.google.inject.Inject;
 import com.google.inject.Injector;
 
 /**
- * Wrapper class to pass the injector in PreprocessingUtil without the warning.
+ * Wrapper class to pass the injector in PreprocessingUtil without the warning:
+ * AssistedInject factory ...will be slow because class ... has assisted Provider dependencies or injects the Injector.
  */
 public class PreprocessingInjectorWrapper {
 
