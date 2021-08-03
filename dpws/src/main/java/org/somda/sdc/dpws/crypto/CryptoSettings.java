41adf560a89c751992b0e7c017934776e066598e
Malte Grebe <malte.grebe@gmx.de>
2021-08-03 11:55:03 +0200

    #206 - removed file support from CryptoSettings

--- dpws/src/main/java/org/somda/sdc/dpws/crypto/CryptoSettings.java
+++ dpws/src/main/java/org/somda/sdc/dpws/crypto/CryptoSettings.java
@@ -1,27 +1,21 @@
 package org.somda.sdc.dpws.crypto;
 
-import java.io.File;
 import java.io.InputStream;
 import java.util.Optional;
 
 /**
  * Common interface to retrieve key and trust store information.
  * <p>
- * Depending on the data source key and trust store information can be referenced using a file
- * ({@link #getKeyStoreFile()}, {@link #getTrustStoreFile()}) or streams ({@link #getKeyStoreStream()},
+ * Key and trust store information can be referenced using streams ({@link #getKeyStoreStream()},
  * {@link #getKeyStoreStream()}).
  *
  * @see <a href="https://www.cloudera.com/documentation/enterprise/5-10-x/topics/cm_sg_create_key_trust.html"
  * >More information on key and trust stores</a>
  */
 public interface CryptoSettings {
-    Optional<File> getKeyStoreFile();
-
     Optional<InputStream> getKeyStoreStream();
 
     String getKeyStorePassword();
-
-    Optional<File> getTrustStoreFile();
 
     Optional<InputStream> getTrustStoreStream();
 
