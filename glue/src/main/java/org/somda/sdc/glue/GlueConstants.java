e932c53f7af3116ae402f2455acc5b398cc33374
Malte Grebe <malte.grebe@gmx.de>
2022-02-18 10:58:01 +0100

    #190 - changed regex for IPV4 address

--- glue/src/main/java/org/somda/sdc/glue/GlueConstants.java
+++ glue/src/main/java/org/somda/sdc/glue/GlueConstants.java
@@ -60,7 +60,7 @@ public class GlueConstants {
     public static final String SEGMENT_NZ_REGEX = P_CHAR + "+";
     public static final String SEGMENT_REGEX = P_CHAR + "*";
     private static final String DEC_OCTET = "((1[0-9][0-9])|(2[0-4][0-9])|(25[0-5])|([0-9])|([1-9][0-9]))";
-    private static final String IPV4_ADDRESS = String.join(".", Collections.nCopies(4, DEC_OCTET));
+    public static final String IPV4_ADDRESS = String.join("\\.", Collections.nCopies(4, DEC_OCTET));
     private static final String REG_NAME = "(?:(?:%[a-fA-F0-9]{2})+|(?:" + "[a-zA-Z0-9\\-._~!$&'()*+,;=]" + ")+)*";
     private static final String USER_INFO = "(?:(?:%[a-fA-F0-9]{2})+|(?:" + "[a-zA-Z0-9\\-._~!$&'()*+,;=:]" + ")+)*";
     private static final String HEXDIG = "[a-fA-F0-9]";
